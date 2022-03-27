08d02547f59195b8c8f7fc68696abfd9e252098f
itheima <itheima@itcast.cn>
2022-03-27 11:00:42 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,49 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+// A模块
+const moduleA = {
+  namespaced: true,
   state: {
-  },
-  getters: {
+    username: 'moduleA'
   },
   mutations: {
+    updateName(state) {
+      state.username = 'moduleAAAAAA'
+    }
+  },
+  getters: {
+    newName(state) {
+      return state.username + '!!!'
+    }
+  }
+}
+// B模块
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'moduleB'
+  },
+  getters: {
+    newNames(state) {
+      return state.username + '!!!'
+    }
+  },
+  mutations: {
+    updateName(state) {
+      state.username = '李四'
+    }
   },
   actions: {
-  },
+    initList(context) {
+      setTimeout(() => {
+        context.commit('updateName')
+      }, 1000)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
